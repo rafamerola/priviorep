cd8171c46cdc95a5da4f8321b350ee0fa0563acc
Rafael Merola Alves <rma@ic.ufal.br>
2020-07-13 20:15:47 -0300

    Privio Lab
    Upload this part of test a new job

---
+++ app/Usuarios.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+
+class Usuarios extends Authenticatable implements  JWTSubject
+{
+
+    protected $fillable = [
+        'nome', 'senha',
+    ];
+
+    public function getJWTIdentifier()
+    {
+        // TODO: Implement getJWTIdentifier() method.
+    }
+
+    public function getJWTCustomClaims()
+    {
+        // TODO: Implement getJWTCustomClaims() method.
+    }
+}
